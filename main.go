4926744526f54dd5d89d930a93d7cda6e9ffe5e4
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-09-11 16:30:08 +0900

    add: sudo check

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log/slog"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -12,7 +11,7 @@ import (
 
 // greeting displays a welcome message and asks the user for confirmation
 // to continue with the installation.
-func greeting() {
+func greeting() bool {
 	fmt.Println("Welcome to the NEVIRAIDE installer!")
 	fmt.Println("This script will check for required dependencies and install them if they're missing.")
 	fmt.Println("It will also set up the NEVIRAIDE configuration for Neovim.")
@@ -20,12 +19,17 @@ func greeting() {
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Would you like to continue with the installation? [y/n]: ")
-	answer, _ := reader.ReadString('\n')
+	answer, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		os.Exit(1)
+	}
 	answer = strings.TrimSpace(answer)
 	if answer != "y" {
 		fmt.Println("Installation canceled by the user.")
-		os.Exit(0)
+		return false
 	}
+	return true
 }
 
 // checkCommandsAvailability checks if the required commands are available
@@ -41,17 +45,34 @@ func checkCommandsAvailability(names map[string]string) []string {
 	return missing
 }
 
+// checkSudo checks if sudo is available
+func checkSudo() bool {
+	cmd := exec.Command("command", "-v", "sudo")
+	return cmd.Run() == nil
+}
+
 // installWithPacman installs the provided package using pacman.
-// Logs an error if the installation fails.
-func installWithPacman(pkg string) {
+func installWithPacman(pkg string) bool {
+	fmt.Printf("Installing %s...\\n", pkg)
 	cmd := exec.Command("sudo", "pacman", "-S", "--noconfirm", pkg)
-	if err := cmd.Run(); err != nil {
-		slog.Error("Installation error %s: %v", pkg, err)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Installation error for %s: %v\\n", pkg, err)
+		return false
 	}
+	fmt.Printf("%s was installed successfully!\\n", pkg)
+	return true
 }
 
 func main() {
-	greeting()
+	if !greeting() {
+		os.Exit(0)
+	}
+
+	if !checkSudo() {
+		fmt.Println("sudo is not available or you don't have sudo privileges.")
+		os.Exit(1)
+	}
 
 	dependencies := map[string]string{
 		"neovim":  "nvim",
@@ -67,21 +88,27 @@ func main() {
 
 	missingDeps := checkCommandsAvailability(dependencies)
 	for _, dep := range missingDeps {
-		fmt.Printf("%s not found. Installing %s...\n", dep, dep)
-		installWithPacman(dep)
+		success := installWithPacman(dep)
+		if !success {
+			fmt.Printf("Failed to install %s. Aborting installation.\\n", dep)
+			os.Exit(1)
+		}
 	}
 
 	repoURL := "https://github.com/RAprogramm/NEVIRAIDE.git"
 	cloneDir := "/tmp/neovim-config"
 
-	_, err := exec.Command("git", "clone", repoURL, cloneDir).Output()
+	fmt.Println("Cloning NEVIRAIDE repository...")
+	_, err := exec.Command("git", "clone", "--depth", "1", repoURL, cloneDir).Output()
 	if err != nil {
-		slog.Error("Cloning repository error: %v", err)
+		fmt.Printf("Cloning repository error: %v\\n", err)
+		return
 	}
+	fmt.Println("Repository cloned successfully!")
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		slog.Error("Error getting home directory: %v", err)
+		fmt.Printf("Error getting home directory: %v\\n", err)
 		return
 	}
 	configDir := filepath.Join(homeDir, ".config/nvim")
@@ -89,29 +116,41 @@ func main() {
 	if _, err = os.Stat(configDir); !os.IsNotExist(err) {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("~/.config/nvim already exists. Remove it? [y/n]: ")
-		answer, _ := reader.ReadString('\n')
+		answer, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			os.Exit(1)
+		}
 		answer = strings.TrimSpace(answer)
 
 		switch answer {
 		case "y":
 			err = os.RemoveAll(configDir)
 			if err != nil {
-				slog.Error("Error removing directory: %v", err)
+				fmt.Printf("Error removing directory: %v\\n", err)
 			}
 		case "n":
 			err = os.Rename(configDir, configDir+".old")
 			if err != nil {
-				slog.Error("Error renaming directory: %v", err)
+				fmt.Printf("Error renaming directory: %v\\n", err)
 			}
 		default:
-			slog.Warn("Undefined choice. Abort installation.")
+			fmt.Println("Undefined choice. Abort installation.")
+			os.Exit(0)
 		}
 	}
 
+	// Ensure the destination directory exists
+	os.MkdirAll(configDir, os.ModePerm)
+
 	err = exec.Command("cp", "-r", cloneDir, configDir).Run()
 	if err != nil {
-		slog.Error("Error copying configuration files: %v", err)
+		fmt.Printf("Error copying configuration files: %v\\n", err)
+		return
 	}
+
+	// Cleaning up the temporary directory
+	os.RemoveAll(cloneDir)
 
 	fmt.Println("NEVIRAIDE was successfully installed!")
 }
