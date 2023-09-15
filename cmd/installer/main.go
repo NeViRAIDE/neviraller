e272e36e74f7730575a11ae95e6cd6f7274a54c1
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-09-15 16:35:02 +0900

    errors handling

--- cmd/installer/main.go
+++ cmd/installer/main.go
@@ -18,19 +18,17 @@ func requestSudo() {
 	}
 }
 
-// greeting displays a welcome message and asks the user for confirmation
-// to continue with the installation.
 func greeting() bool {
 	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Println("Welcome to the NEVIRAIDE installer!")
 	fmt.Println("This script will check for required dependencies and install them if they're missing.")
 	fmt.Println("It will also set up the NEVIRAIDE configuration for Neovim.")
-	fmt.Println("")
 
-	fmt.Print("Would you like to install latest pre-release of Neovim nighly? [y/n]: ")
+	fmt.Print("Would you like to install the latest pre-release of Neovim Nightly? [y/n]: ")
 	answer1, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error reading input:", err)
+		fmt.Printf("Error reading input: %v\n", err)
 		os.Exit(1)
 	}
 	answer1 = strings.TrimSpace(answer1)
@@ -41,7 +39,7 @@ func greeting() bool {
 	fmt.Print("Would you like to continue with the installation? [y/n]: ")
 	answer2, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error reading input:", err)
+		fmt.Printf("Error reading input: %v\n", err)
 		os.Exit(1)
 	}
 	answer2 = strings.TrimSpace(answer2)
@@ -52,8 +50,6 @@ func greeting() bool {
 	return true
 }
 
-// checkCommandsAvailability checks if the required commands are available
-// Returns a slice of missing commands.
 func checkCommandsAvailability(names map[string]string) []string {
 	missing := []string{}
 	for name, cmd := range names {
@@ -65,28 +61,26 @@ func checkCommandsAvailability(names map[string]string) []string {
 	return missing
 }
 
-// checkSudo checks if sudo is available
 func checkSudo() bool {
 	cmd := exec.Command("command", "-v", "sudo")
-	return cmd.Run() != nil
+	return cmd.Run() == nil
 }
 
-// installWithPacman installs the provided package using pacman.
 func installWithPacman(pkg string) bool {
-	fmt.Printf("Installing %s...\\n", pkg)
+	fmt.Printf("Installing %s...\n", pkg)
 	cmd := exec.Command("sudo", "pacman", "-S", "--noconfirm", pkg)
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("Installation error for %s: %v\\n", pkg, err)
+		fmt.Printf("Installation error for %s: %v\n", pkg, err)
 		return false
 	}
-	fmt.Printf("%s was installed successfully!\\n", pkg)
+	fmt.Printf("%s was installed successfully!\n", pkg)
 	return true
 }
 
 func main() {
 	if !greeting() {
-		os.Exit(0)
+		return
 	}
 
 	requestSudo()
@@ -97,9 +91,6 @@ func main() {
 	}
 
 	dependencies := map[string]string{
-		// TODO: neovim nightly form script
-
-		// "neovim":  "nvim",
 		"git":     "git",
 		"ripgrep": "rg",
 		"fd":      "fd",
@@ -114,7 +105,7 @@ func main() {
 	for _, dep := range missingDeps {
 		success := installWithPacman(dep)
 		if !success {
-			fmt.Printf("Failed to install %s. Aborting installation.\\n", dep)
+			fmt.Printf("Failed to install %s. Aborting installation.\n", dep)
 			os.Exit(1)
 		}
 	}
@@ -125,14 +116,14 @@ func main() {
 	fmt.Println("Cloning NEVIRAIDE repository...")
 	_, err := exec.Command("git", "clone", "--depth", "1", repoURL, cloneDir).Output()
 	if err != nil {
-		fmt.Printf("Cloning repository error: %v\\n", err)
+		fmt.Printf("Cloning repository error: %v\n", err)
 		return
 	}
 	fmt.Println("Repository cloned successfully!")
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		fmt.Printf("Error getting home directory: %v\\n", err)
+		fmt.Printf("Error getting home directory: %v\n", err)
 		return
 	}
 	configDir := filepath.Join(homeDir, ".config/nvim")
@@ -140,9 +131,9 @@ func main() {
 	if _, err = os.Stat(configDir); !os.IsNotExist(err) {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("~/.config/nvim already exists. Remove it? [y/n]: ")
-		answer, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading input:", err)
+		answer, err1 := reader.ReadString('\n')
+		if err1 != nil {
+			fmt.Printf("Error reading input: %v\n", err1)
 			os.Exit(1)
 		}
 		answer = strings.TrimSpace(answer)
@@ -151,12 +142,14 @@ func main() {
 		case "y":
 			err = os.RemoveAll(configDir)
 			if err != nil {
-				fmt.Printf("Error removing directory: %v\\n", err)
+				fmt.Printf("Error removing directory: %v\n", err)
+				os.Exit(1)
 			}
 		case "n":
 			err = os.Rename(configDir, configDir+".old")
 			if err != nil {
-				fmt.Printf("Error renaming directory: %v\\n", err)
+				fmt.Printf("Error renaming directory: %v\n", err)
+				os.Exit(1)
 			}
 		default:
 			fmt.Println("Undefined choice. Abort installation.")
@@ -164,16 +157,12 @@ func main() {
 		}
 	}
 
-	// Ensure the destination directory exists
-	os.MkdirAll(configDir, os.ModePerm)
-
 	err = exec.Command("cp", "-r", cloneDir, configDir).Run()
 	if err != nil {
-		fmt.Printf("Error copying configuration files: %v\\n", err)
+		fmt.Printf("Error copying configuration files: %v\n", err)
 		return
 	}
 
-	// Cleaning up the temporary directory
 	os.RemoveAll(cloneDir)
 
 	fmt.Println("NEVIRAIDE was successfully installed!")
--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.21.1
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/alexeyco/simpletable v1.0.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
